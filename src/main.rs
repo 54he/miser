4b3894d96fc0ff2cbd6b2f2c6bde00362f99de0e
awahe4600 <win7yyds2022@outlook.com>
2025-01-08 21:28:01 +0800

    备份pwp 使用mime

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-#![allow(unstable_name_collisions)]
 #![feature(panic_payload_as_str)]
 use {
     chrono::Local,
+    mime_guess::MimeGuess,
     std::{
         fs::{self, OpenOptions},
         io::Write,
+        path::Path,
+        collections::HashMap,
     },
     tokio::{
         io::{AsyncReadExt, AsyncWriteExt,BufReader},
@@ -20,15 +22,16 @@ const NOT_FOUND: &str = "HTTP/1 404 Not Found";
 const MOVED_PERMANENTLY: &str = "HTTP/1 302 Found";
 const ROOT_DIR: &str = "/server";
 const LOG_FILE: &str = "/var/log/minser/connect.log";
+
 #[tokio::main]
 async fn main() {
     loop {
         tokio::spawn(async move {
             main_process().await;
         })
-        .await
+        .await        
         .unwrap();
-    }
+   }
 }
 async fn main_process() {
     std::panic::set_hook(Box::new(|panic_info| {
@@ -71,10 +74,14 @@ async fn handle_client(socket: TcpStream) {
             break;
         }
         let client_read = String::from_utf8_lossy(&buf).to_string();
-    
-        let get = format!("{}", (read_response(client_read)));
-        let get = read_realget(get);
-        let last_char = get.clone().pop().expect("can not get lastchar");
+        //推荐传递使用str传到内部用string因为它不支持copy
+        let get = response_reader(client_read).expect("4: RN cant read reponse");
+        let get = read_realget(get.get("GET").unwrap());
+        let get=get.as_str();
+        
+        
+        let last_char = get.to_string().pop().expect("can not get lastchar");
+        
         let list = match fs::metadata(format!("{ROOT_DIR}{get}")) {
             Ok(n) => {
                 if n.is_dir() && (last_char == '/') {
@@ -103,6 +110,11 @@ async fn handle_client(socket: TcpStream) {
 }
 async fn response(url: &str, status_line: &str) -> Vec<u8> {
     //info_log(file);
+    
+    let mime_type_guess=if let Some(n) = MimeGuess::from_path(Path::new(&url)).first(){
+     format!("{}", n)
+    }else{"text/html".into()};
+    
     let (contents, is_http_hender): (Vec<u8>, bool) = match status_line {
         OK | NOT_FOUND => match fs::read(url) {
             Ok(n) => (n, false),
@@ -111,13 +123,13 @@ async fn response(url: &str, status_line: &str) -> Vec<u8> {
         MOVED_PERMANENTLY => (format!("Location: {url}\r\n\r\n").into(), true),
         _ => panic!("4: cant understand the http status_line "),
     };
-
     let mut response_vec = if is_http_hender {
         format!("{status_line}\r\n")
     } else {
         format!(
-            "{status_line}\r\nContent-Length: {}\r\n\r\n",
-            contents.len()
+            "{status_line}\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n",
+            mime_type_guess,
+            contents.len(),
         )
     }
     .as_bytes()
@@ -128,9 +140,27 @@ async fn response(url: &str, status_line: &str) -> Vec<u8> {
 }
 /*fn merge_vec_u8(a: Vec<u8>, b: Vec<u8>) -> Vec<u8> {
     a.into_iter().chain(b.into_iter()).collect()
-}*/
-//read_response函数能正常运行就别碰
-fn read_response(response: String) -> String {
+}*/ //旧方法
+fn response_reader(response: String) -> Option<HashMap<String,String>>{
+    let mut headers: HashMap<String, String> = HashMap::new();
+    let mut reponse_line =response.lines();
+    let mut http_reponse_line = reponse_line.next()?.split_whitespace();
+    let (http_method,resource_path,http_version)=(
+      http_reponse_line.next()?,
+      http_reponse_line.next()?,
+      http_reponse_line.next()?
+    );
+    headers.insert(http_method.to_string(),resource_path.to_string());
+    headers.insert("Http-Version".to_string(),http_version.to_string());
+    for respose_heander in reponse_line{
+      if let Some( (key,value) ) = respose_heander.split_once(": "){
+             headers.insert(key.to_string(),value.to_string());
+      }
+    }
+   Some(headers)
+}
+//read_response函数能正常运行就别碰纯史山旧方法
+/*fn read_response(response: String) -> String {
     ((((&(((response
         .split_once('\n')
         .expect(format!("3_RD :cant read response from '{}' ", response).as_str()))
@@ -141,24 +171,24 @@ fn read_response(response: String) -> String {
     .unwrap())
     .0)
         .to_string()
-}
-fn read_realget(response: String) -> String {
+}*/
+fn read_realget(response: &str) -> String {
     if let Some(index) = response.find('?') {
         (&response[..index]).to_string()
     } else {
-        response
+        response.to_string()
     }
 }
 
 fn err_log<T: std::fmt::Display>(message: T) {
     //这才配得上我们最高规格的日志!!
-    write_log(message, "======@#!!!!!ERROR!!!!!#@=====\n   @^ERR=>", true).unwrap();
+    write_log(message, "======@#!!!!!ERROR!!!!!#@=====\n   @^ERR=>", true);
 }
 fn wran_log<T: std::fmt::Display>(message: T) {
-    write_log(message, "WRAN", false).unwrap();
+    write_log(message, "WRAN", false);
 }
 fn info_log<T: std::fmt::Display>(message: T) {
-    write_log(message, "info", false).unwrap();
+    write_log(message, "info", false);
 }
 fn write_log<T: std::fmt::Display>(
     //报错详细内容
@@ -168,7 +198,7 @@ fn write_log<T: std::fmt::Display>(
     mode_info: &str,
     //是否使用eprintln即作为错误信息输出 真即使用 假即使用println
     mode: bool,
-	) -> Result<(), Box<dyn std::error::Error>> {
+	){
     let write_temp = format!(
         "{mode_info}[{}]:{}\n",
         Local::now().format("%Y-%m-%d %H:%M:%S"),
@@ -181,6 +211,6 @@ fn write_log<T: std::fmt::Display>(
         print!("{}", write_temp)
     }
     //写入日志
-    write!(OpenOptions::new().append(true).open(LOG_FILE)?, "{}", write_temp)?;
-    Ok(())
+    write!(OpenOptions::new().append(true).open(LOG_FILE).expect("4: cant write to log file"), "{}", write_temp).expect("4: cant write to log file");
+
 }
